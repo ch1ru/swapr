acd7552fe83d4cd3dd4dc352254be34a41a55ae2
kazute <kazute@kazute.com>
2023-02-18 10:07:15 +0000

    get params

--- backend/nodejs/src/controller/users.controller.ts
+++ backend/nodejs/src/controller/users.controller.ts
@@ -6,16 +6,41 @@ import user from '../../models/user';
 
 export const users = async (req: Request, res: Response): Promise<Response<any>> => {
     try {
+        
         const users = await user.find();
           
           console.log(users);
 
           return res.status(Code.OK)
-          .send(new HttpResponse(Code.OK, Status.OK, 'Created user', users));
+          .send(new HttpResponse(Code.OK, Status.OK, 'Found users', users));
     }
     catch(error: unknown) {
         console.log(error);
         return res.status(Code.INTERNAL_SERVER_ERROR)
           .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, "Internal server error"));
+    }
+}
+
+export const locateUser = async (req: Request, res: Response): Promise<Response<any>> => {
+    try {
+        console.log(req.params);
+        if(!req.params.username) {
+            return res.status(Code.BAD_REQUEST)
+            .send(new HttpResponse(Code.BAD_REQUEST, Status.BAD_REQUEST, "Search parameters missing"));
+        }
+
+        const searchparam = req.params.username;
+        const query = { username: searchparam };
+        const findUser = user.find(query);
+
+        console.log(findUser);
+
+        return res.status(Code.OK)
+          .send(new HttpResponse(Code.OK, Status.OK, 'Found user', findUser));
+    }
+    catch(error: unknown) {
+        console.log(error);
+        return res.status(Code.INTERNAL_SERVER_ERROR)
+        .send(new HttpResponse(Code.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR, "Internal server error"));
     }
 }
--- backend/nodejs/src/routes/user.routes.ts
+++ backend/nodejs/src/routes/user.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
-import { users } from '../controller/users.controller';
+import { locateUser, users } from '../controller/users.controller';
 
 const userRoutes = Router();
 
 userRoutes.route('/')
   .get(users);
+
+userRoutes.route('/locate')
+  .get(locateUser);
+userRoutes.route('/locate/:username')
+  .get(locateUser);
 
 export default userRoutes;
